73b1698355317fd50eab72ae1dda9f097481107e
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-07-30 20:31:34 +0530

    saasd

--- scrapo/scrapy.py
+++ scrapo/scrapy.py
@@ -1,3 +1,16 @@
 import scrapy
 
-class Scrapy(scrapy.Spyder):
+class Scrapy(scrapy.Spider):
+    name = "books"
+    start_url = [
+        
+        "https://books.toscrape.com/",
+        "https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html",
+        
+        ]
+
+    def parse(self,response):
+        page = response.url.split('/')[-2]
+        filename = 'books-%s' % page
+        
+    
